74b9916a1764787be6b2d9a791c7578ec77ebf43
zoyitov17 <adhamjonzokhidov@gmail.com>
2024-07-15 07:32:43 +0500

    all done

--- src/App.jsx
+++ src/App.jsx
@@ -9,25 +9,13 @@ import {
 import Home from "./components/home/Home";
 import Products from "./components/products/Products";
 import Product from "./components/product/Product";
-import Cart from "./components/card/Card"; 
+import Cart from "./components/card/Card";
 
 function App() {
   const [cart, setCart] = useState([]);
 
   const addToCart = (product) => {
-    const existingProductIndex = cart.findIndex(
-      (item) => item.id === product.id
-    );
-
-    if (existingProductIndex >= 0) {
-      // If yes, update quantity
-      const updatedCart = [...cart];
-      updatedCart[existingProductIndex].quantity += 1;
-      setCart(updatedCart);
-    } else {
-      // If no, add product with quantity 1
-      setCart([...cart, { ...product, quantity: 1 }]);
-    }
+    setCart([...cart, product]);
   };
 
   return (
@@ -37,19 +25,27 @@ function App() {
           <nav className="navbar">
             <ul>
               <li>
-                <NavLink activeClassName="activeLink" to="/">
+                <NavLink
+                  className={({ isActive }) => (isActive ? "activeLink" : "")}
+                  to="/"
+                >
                   Home
                 </NavLink>
               </li>
               <li>
-                <NavLink activeClassName="activeLink" to="/products">
+                <NavLink
+                  className={({ isActive }) => (isActive ? "activeLink" : "")}
+                  to="/products"
+                >
                   Products
                 </NavLink>
               </li>
               <li>
-                <NavLink activeClassName="activeLink" to="/cart">
-                  Cart ({cart.reduce((total, item) => total + item.quantity, 0)}
-                  )
+                <NavLink
+                  className={({ isActive }) => (isActive ? "activeLink" : "")}
+                  to="/cart"
+                >
+                  Cart ({cart.length})
                 </NavLink>
               </li>
             </ul>
@@ -59,16 +55,15 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route
             path="/products"
-            element={<Products addToCart={addToCart} />} // Pass addToCart directly to Products component
+            element={
+              <Products cart={cart} setCart={setCart} addToCart={addToCart} />
+            }
           />
           <Route
             path="/products/:productId"
             element={<Product addToCart={addToCart} />}
           />
-          <Route
-            path="/cart"
-            element={<Cart cart={cart} />} // Pass cart state to Cart component
-          />
+          <Route path="/cart" element={<Cart cart={cart} />} />
         </Routes>
       </Router>
     </div>
